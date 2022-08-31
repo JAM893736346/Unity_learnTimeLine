3d4ba97f5878e22847b17d139ed733499109ddd1
chenjiyang <893736346@qq.com>
2022-08-31 18:56:53 +0800

    8/31-last

--- Assets/DefaultPlayables/LightControl/LightControlClip.cs
+++ Assets/DefaultPlayables/LightControl/LightControlClip.cs
@@ -6,6 +6,7 @@ using UnityEngine.Timeline;
 [Serializable]
 public class LightControlClip : PlayableAsset, ITimelineClipAsset
 {
+    // 将抽象函数中的接收的第一个参数以及添加的LightControlBehaviour类型对象一起传入并调用ScriptPlayable.Create函数，从而得到一个跟LightControlBehaviour类型的行为相绑定的可播放资源类型对象。
     public LightControlBehaviour template = new LightControlBehaviour ();
 
     public ClipCaps clipCaps
